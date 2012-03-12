0a7087808b544d8b36eb682bf58136bf111b6d32
sue445 <sue445@sue445.net>
2012-03-13 06:53:26 +0900

    refactor test: use Enclosed

--- src/test/java/net/sue445/kulib/dao/MemcacheProxyDaoBaseTest.java
+++ src/test/java/net/sue445/kulib/dao/MemcacheProxyDaoBaseTest.java
@@ -4,129 +4,130 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 import net.sue445.kulib.model.DummyModel;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
 import org.slim3.datastore.Datastore;
 import org.slim3.memcache.Memcache;
 import org.slim3.tester.AppEngineTestCase;
 
 import com.google.appengine.api.datastore.Key;
 
-
-public class MemcacheProxyDaoBaseTest extends AppEngineTestCase{
-	private MemcacheProxyDaoBase<DummyModel> dao = new MemcacheProxyDaoBase<DummyModel>(){
-	};
-
+@RunWith(Enclosed.class)
+public class MemcacheProxyDaoBaseTest{
 	private static final String CURRENT_VERSION_ID = "1.0";
 
+	private static MemcacheProxyDaoBase<DummyModel> dao = new MemcacheProxyDaoBase<DummyModel>(){
+	};
 
-	@Test
-	public void test() throws Exception {
-		assertThat(dao, is(notNullValue()));
+	public static class WhenNotExistsData extends AppEngineTestCase{
+		@Test
+		public void test() throws Exception {
+			assertThat(dao, is(notNullValue()));
+		}
+
+		@Test
+		public void createMemcacheKey(){
+			Key key = Datastore.createKey(DummyModel.class, 1);
+			assertThat(dao.createMemcacheKey(key), is(CURRENT_VERSION_ID + "DummyModel(1)"));
+		}
+
+		@Test
+		public void put(){
+			DummyModel model = createDefaultModel();
+			dao.put(model);
+
+			Key datastoreKey = createDefaultDatastoreKey();
+			DummyModel actual = Datastore.get(DummyModel.class, datastoreKey);
+			assertThat(actual, is(notNullValue()));
+			assertThat(actual.getKey(), is(datastoreKey));
+			assertThat(actual.getData(), is("100"));
+
+			String memcacheKey = createDefaultMemcacheKey();
+			assertThat(Memcache.contains(memcacheKey), is(false));
+		}
+
+		@Test
+		public void get(){
+			DummyModel actual = dao.get(createDefaultDatastoreKey());
+			assertThat(actual, is(nullValue()));
+		}
 	}
 
-	@Test
-	public void createMemcacheKey(){
-		Key key = Datastore.createKey(DummyModel.class, 1);
-		assertThat(dao.createMemcacheKey(key), is(CURRENT_VERSION_ID + "DummyModel(1)"));
+	public static class WhenExistsOnlyDatastore extends AppEngineTestCase{
+		@Before
+		public void setUpDatastore() {
+			DummyModel model = createDefaultModel();
+			Datastore.put(model);
+		}
+
+		@Test
+		public void get(){
+			DummyModel datastoreActual = dao.get(createDefaultDatastoreKey());
+			assertThat(datastoreActual, is(notNullValue()));
+			assertThat(datastoreActual.getData(), is("100"));
+
+			DummyModel memcacheActual = Memcache.get(createDefaultMemcacheKey());
+			assertThat(memcacheActual, is(notNullValue()));
+			assertThat(memcacheActual.getData(), is("100"));
+		}
+
 	}
 
-	@Test
-	public void put(){
-		DummyModel model = createDefaultModel();
-		dao.put(model);
+	public static class WhenExistsOnlyMemcache extends AppEngineTestCase{
+		@Before
+		public void setUpMemcache() {
+			Memcache.put(createDefaultMemcacheKey(), createDefaultModel());
+		}
 
-		Key datastoreKey = createDefaultDatastoreKey();
-		DummyModel actual = Datastore.get(DummyModel.class, datastoreKey);
-		assertThat(actual, is(notNullValue()));
-		assertThat(actual.getKey(), is(datastoreKey));
-		assertThat(actual.getData(), is("100"));
+		@Test
+		public void get_FoundInMemcache(){
+			setUpMemcache();
 
-		String memcacheKey = createDefaultMemcacheKey();
-		assertThat(Memcache.contains(memcacheKey), is(false));
+			DummyModel memcacheActual = dao.get(createDefaultDatastoreKey());
+			assertThat(memcacheActual, is(notNullValue()));
+			assertThat(memcacheActual.getData(), is("100"));
+		}
 	}
 
-	private DummyModel createDefaultModel() {
+	public static class WhenExistsDatastoreAndMemcache extends AppEngineTestCase{
+		@Before
+		public void setUpDatastore() {
+			DummyModel model = createDefaultModel();
+			Datastore.put(model);
+		}
+
+		@Before
+		public void setUpMemcache() {
+			Memcache.put(createDefaultMemcacheKey(), createDefaultModel());
+		}
+
+		@Test
+		public void delete() throws Exception {
+			dao.delete(createDefaultDatastoreKey());
+
+			assertThat(tester.count(DummyModel.class), is(0));
+			assertThat(Memcache.contains(createDefaultMemcacheKey()), is(false));
+		}
+	}
+
+	private static DummyModel createDefaultModel() {
+		return createModel("name", "100");
+	}
+
+	private static DummyModel createModel(String name, String value) {
 		DummyModel model = new DummyModel();
-		model.setKey(createDefaultDatastoreKey());
-		model.setData("100");
+		model.setKey(Datastore.createKey(DummyModel.class, name));
+		model.setData(value);
 		return model;
 	}
 
-	private Key createDefaultDatastoreKey() {
+	private static Key createDefaultDatastoreKey() {
 		return Datastore.createKey(DummyModel.class, "name");
 	}
 
-	private String createDefaultMemcacheKey() {
+	private static String createDefaultMemcacheKey() {
 		return dao.createMemcacheKey(createDefaultDatastoreKey());
-	}
-
-	@Test
-	public void get_NotFound(){
-		DummyModel actual = dao.get(createDefaultDatastoreKey());
-		assertThat(actual, is(nullValue()));
-	}
-
-	@Test
-	public void get_FoundInDatastore(){
-		setUpDatastoreModel();
-
-		DummyModel datastoreActual = dao.get(createDefaultDatastoreKey());
-		assertThat(datastoreActual, is(notNullValue()));
-		assertThat(datastoreActual.getData(), is("100"));
-
-		DummyModel memcacheActual = Memcache.get(createDefaultMemcacheKey());
-		assertThat(memcacheActual, is(notNullValue()));
-		assertThat(memcacheActual.getData(), is("100"));
-	}
-
-	private void setUpDatastoreModel() {
-		DummyModel model = createDefaultModel();
-		Datastore.put(model);
-	}
-
-	@Test
-	public void get_FoundInMemcache(){
-		beforeGet_FoundInMemcache();
-		setUpMemcacheModel();
-
-		DummyModel memcacheActual = dao.get(createDefaultDatastoreKey());
-		assertThat(memcacheActual, is(notNullValue()));
-		assertThat(memcacheActual.getData(), is("100"));
-	}
-
-	private void beforeGet_FoundInMemcache() {
-		DummyModel datastoreModel = new DummyModel();
-		datastoreModel.setKey(createDefaultDatastoreKey());
-		datastoreModel.setData("200");	// dummy
-		Datastore.put(datastoreModel);
-	}
-
-	private void setUpMemcacheModel() {
-		Memcache.put(createDefaultMemcacheKey(), createDefaultModel());
-	}
-
-	@Test
-	public void getModelOrNull_Found(){
-		setUpDatastoreModel();
-
-		DummyModel actual = Datastore.getOrNull(dao.m, createDefaultDatastoreKey());
-		assertThat(actual, is(notNullValue()));
-	}
-
-	@Test
-	public void getModelOrNull_NotFound(){
-		DummyModel actual = Datastore.getOrNull(dao.m, createDefaultDatastoreKey());
-		assertThat(actual, is(nullValue()));
-	}
-
-	@Test
-	public void delete() throws Exception {
-		setUpDatastoreModel();
-		setUpMemcacheModel();
-
-		dao.delete(createDefaultDatastoreKey());
-
-		assertThat(tester.count(DummyModel.class), is(0));
-		assertThat(Memcache.contains(createDefaultMemcacheKey()), is(false));
 	}
 }
