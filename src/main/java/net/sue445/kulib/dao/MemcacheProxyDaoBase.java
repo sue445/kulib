d08b9c746520397e19d03ad6e4ebb760845fee89
sue445 <sue445@sue445.net>
2012-03-13 21:39:42 +0900

    impl xxxAll
    impl deleteAll
    
    impl putAll
    
    WIP
    
    impl getAll
    
    add test case

--- src/main/java/net/sue445/kulib/dao/MemcacheProxyDaoBase.java
+++ src/main/java/net/sue445/kulib/dao/MemcacheProxyDaoBase.java
@@ -2,6 +2,11 @@ package net.sue445.kulib.dao;
 
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -60,8 +65,21 @@ public abstract class MemcacheProxyDaoBase<M extends Slim3Model> {
 	 */
 	public void put(M model){
 		deleteInMemcache(model.getKey());
-
 		Datastore.putAsync(model);
+	}
+
+	/**
+	 * put models and clear Memcache
+	 * @param model
+	 */
+	public void putAll(Iterable<M> models){
+		List<Key> keys = new ArrayList<Key>();
+		for(M model : models){
+			keys.add(model.getKey());
+		}
+
+		deleteAllInMemcache(keys);
+		Datastore.putAsync(models);
 	}
 
 	/**
@@ -81,12 +99,37 @@ public abstract class MemcacheProxyDaoBase<M extends Slim3Model> {
 		return ApiProxy.getCurrentEnvironment().getVersionId();
 	}
 
+	/**
+	 *
+	 * @param keys
+	 * @return
+	 */
+	protected List<String> createMemcacheKeys(Iterable<Key> keys){
+		List<String> memcacheKeyList = new ArrayList<String>();
+
+		for(Key key : keys){
+			memcacheKeyList.add(createMemcacheKey(key));
+		}
+
+		return memcacheKeyList;
+	}
+
 	protected void deleteInMemcache(Key key) {
 		String memcacheKey = createMemcacheKey(key);
 		try {
 			Memcache.delete(memcacheKey);
 		} catch (Exception e) {
 			String message = "[FAILED]Memcache delete:key=" + memcacheKey;
+			logger.log(Level.WARNING, message, e);
+		}
+	}
+
+	protected void deleteAllInMemcache(Iterable<Key> keys) {
+		List<String> memcacheKeys = createMemcacheKeys(keys);
+		try {
+			Memcache.deleteAll(memcacheKeys);
+		} catch (Exception e) {
+			String message = "[FAILED]Memcache delete:key=" + memcacheKeys;
 			logger.log(Level.WARNING, message, e);
 		}
 	}
@@ -148,11 +191,96 @@ public abstract class MemcacheProxyDaoBase<M extends Slim3Model> {
 	}
 
 	/**
-	 * delete models in Datastote and Memcache
+	 *
+	 * @param model
+	 */
+	protected void putAllToMemcache(Iterable<M> models) {
+		Map<Object, Object> modelMap = new LinkedHashMap<Object, Object>();
+
+		for(M model : models){
+			String memcacheKey = createMemcacheKey(model.getKey());
+			modelMap.put(memcacheKey, model);
+		}
+
+		try {
+			Memcache.putAll(modelMap);
+
+		} catch (Exception e) {
+			String message = "[FAILED]Memcache get:key=" + modelMap.keySet();
+			logger.log(Level.WARNING, message, e);
+		}
+	}
+
+	/**
+	 * get model from Memcache or Datastore.<br>
+	 * if not found in Memcache, get from Datastore and put to Memcache.
+	 * @param key
+	 * @return
+	 */
+	public List<M> getAll(Iterable<Key> keys){
+		Map<String, M> memcacheMap = getAllFromMemcache(keys);
+
+		List<Key> datastoreKeys = new ArrayList<Key>();
+		for(Key key : keys){
+			String memcacheKey = createMemcacheKey(key);
+			if(!memcacheMap.containsKey(memcacheKey)){
+				// NotFound in Memcache, and get from Datastore
+				datastoreKeys.add(key);
+			}
+		}
+
+		List<M> datastoreList = Datastore.get(m, datastoreKeys);
+
+		List<M> result = new ArrayList<M>(memcacheMap.values());
+		result.addAll(datastoreList);
+
+		putAllToMemcache(datastoreList);
+
+		return result;
+	}
+
+	/**
+	 *
+	 * @param memcacheKey
+	 * @return
+	 */
+	protected Map<String, M> getAllFromMemcache(Iterable<Key> keys) {
+		List<String> memcacheKeys = createMemcacheKeys(keys);
+		try {
+			Map<Object, Object> memcacheMap = Memcache.getAll(memcacheKeys);
+			Map<String, M> resultMap = new LinkedHashMap<String, M>();
+
+			for(Map.Entry<Object, Object> entry : memcacheMap.entrySet()){
+				@SuppressWarnings("unchecked")
+				M value = (M)entry.getValue();
+
+				resultMap.put((String)entry.getKey(), value);
+			}
+			return resultMap;
+
+		} catch (Exception e) {
+			String message = "[FAILED]Memcache get:key=" + memcacheKeys;
+			logger.log(Level.WARNING, message, e);
+			return null;
+		}
+	}
+
+	/**
+	 * delete model in both Datastote and Memcache
 	 * @param key
 	 */
 	public void delete(Key key){
 		Datastore.deleteAsync(key);
 		deleteInMemcache(key);
 	}
+
+	/**
+	 * delete models in both Datastote and Memcache
+	 * @param key
+	 */
+	public void deleteAll(Iterable<Key> keys){
+		Datastore.deleteAsync(keys);
+		deleteAllInMemcache(keys);
+	}
+
 }
--- src/test/java/net/sue445/kulib/dao/MemcacheProxyDaoBaseTest.java
+++ src/test/java/net/sue445/kulib/dao/MemcacheProxyDaoBaseTest.java
@@ -2,6 +2,10 @@ package net.sue445.kulib.dao;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+
 import net.sue445.kulib.model.DummyModel;
 
 import org.junit.Before;
@@ -16,6 +20,10 @@ import com.google.appengine.api.datastore.Key;
 
 @RunWith(Enclosed.class)
 public class MemcacheProxyDaoBaseTest{
+	private static final String DEFAULT_KEY_NAME = "name";
+	private static final String NAME1 = "name1";
+	private static final String NAME2 = "name2";
+
 	private static final String CURRENT_VERSION_ID = "1.0";
 
 	private static MemcacheProxyDaoBase<DummyModel> dao = new MemcacheProxyDaoBase<DummyModel>(){
@@ -58,76 +66,194 @@ public class MemcacheProxyDaoBaseTest{
 	public static class WhenExistsOnlyDatastore extends AppEngineTestCase{
 		@Before
 		public void setUpDatastore() {
-			DummyModel model = createDefaultModel();
+			putDatastore(NAME1, "100");
+			putDatastore(NAME2, "200");
+		}
+
+		private void putDatastore(String name, String value) {
+			DummyModel model = createModel(name, value);
 			Datastore.put(model);
 		}
 
 		@Test
 		public void get(){
-			DummyModel datastoreActual = dao.get(createDefaultDatastoreKey());
+			DummyModel datastoreActual = dao.get(createDatastoreKey(NAME1));
 			assertThat(datastoreActual, is(notNullValue()));
 			assertThat(datastoreActual.getData(), is("100"));
 
-			DummyModel memcacheActual = Memcache.get(createDefaultMemcacheKey());
+			DummyModel memcacheActual = Memcache.get(createMemcacheKey(NAME1));
 			assertThat(memcacheActual, is(notNullValue()));
 			assertThat(memcacheActual.getData(), is("100"));
 		}
 
+		@Test
+		public void getAll() throws Exception {
+			Key[] keys = {
+					createDatastoreKey(NAME1),
+					createDatastoreKey(NAME2),
+			};
+			List<DummyModel> actual = dao.getAll(Arrays.asList(keys));
+
+			assertThat(actual, is(notNullValue()));
+			assertThat(actual.size(), is(2));
+
+			assertThat(actual.get(0).getKey(), is(createDatastoreKey(NAME1)));
+			assertThat(actual.get(1).getKey(), is(createDatastoreKey(NAME2)));
+
+			assertThat(Memcache.contains(createMemcacheKey(NAME1)), is(true));
+			assertThat(Memcache.contains(createMemcacheKey(NAME2)), is(true));
+		}
 	}
 
 	public static class WhenExistsOnlyMemcache extends AppEngineTestCase{
 		@Before
 		public void setUpMemcache() {
-			Memcache.put(createDefaultMemcacheKey(), createDefaultModel());
+			putMemcache(NAME1, "100");
+			putMemcache(NAME2, "200");
+		}
+
+		private void putMemcache(String name, String value) {
+			DummyModel model = createModel(name, value);
+			Memcache.put(createMemcacheKey(name), model);
 		}
 
 		@Test
 		public void get_FoundInMemcache(){
-			setUpMemcache();
-
-			DummyModel memcacheActual = dao.get(createDefaultDatastoreKey());
+			DummyModel memcacheActual = dao.get(createDatastoreKey(NAME1));
 			assertThat(memcacheActual, is(notNullValue()));
 			assertThat(memcacheActual.getData(), is("100"));
+		}
+
+		@Test
+		public void putAll() throws Exception {
+			DummyModel[] models = {
+					createModel(NAME1, "100"),
+					createModel(NAME2, "200"),
+			};
+			dao.putAll(Arrays.asList(models));
+
+			assertThat(tester.count(DummyModel.class), is(2));
+			assertThat(Memcache.contains(createMemcacheKey(NAME1)), is(false));
+			assertThat(Memcache.contains(createMemcacheKey(NAME2)), is(false));
+		}
+
+		@Test
+		public void getAll() throws Exception {
+			Key[] keys = {
+					createDatastoreKey(NAME1),
+					createDatastoreKey(NAME2),
+			};
+			List<DummyModel> actual = dao.getAll(Arrays.asList(keys));
+
+			assertThat(actual.size(), is(2));
+			assertThat(actual.get(0).getKey(), is(createDatastoreKey(NAME1)));
+			assertThat(actual.get(1).getKey(), is(createDatastoreKey(NAME2)));
 		}
 	}
 
 	public static class WhenExistsDatastoreAndMemcache extends AppEngineTestCase{
-		@Before
-		public void setUpDatastore() {
-			DummyModel model = createDefaultModel();
-			Datastore.put(model);
-		}
 
 		@Before
-		public void setUpMemcache() {
-			Memcache.put(createDefaultMemcacheKey(), createDefaultModel());
+		public void setUpDatastoreAndMemcache() {
+			putDatastoreAndMemcache(NAME1, "100");
+			putDatastoreAndMemcache(NAME2, "200");
+		}
+
+		private void putDatastoreAndMemcache(String name, String value) {
+			DummyModel model = createModel(name, value);
+			Datastore.put(model);
+			Memcache.put(createMemcacheKey(name), model);
 		}
 
 		@Test
 		public void delete() throws Exception {
-			dao.delete(createDefaultDatastoreKey());
+			dao.delete(createDatastoreKey(NAME1));
+
+			assertThat(tester.count(DummyModel.class), is(1));
+			assertThat(Memcache.contains(createMemcacheKey(NAME1)), is(false));
+			assertThat(Memcache.contains(createMemcacheKey(NAME2)), is(true));
+		}
+
+		@Test
+		public void deleteAll() throws Exception {
+			Key[] keys = {
+					createDatastoreKey(NAME1),
+					createDatastoreKey(NAME2),
+			};
+			dao.deleteAll(Arrays.asList(keys));
 
 			assertThat(tester.count(DummyModel.class), is(0));
-			assertThat(Memcache.contains(createDefaultMemcacheKey()), is(false));
+			assertThat(Memcache.contains(createMemcacheKey(NAME1)), is(false));
+			assertThat(Memcache.contains(createMemcacheKey(NAME2)), is(false));
 		}
 	}
 
+	public static class WhenExistsDatastoreOrMemcache extends AppEngineTestCase{
+
+		@Before
+		public void setUpMemcache() {
+			putMemcache(NAME1, "100");
+		}
+
+		private void putMemcache(String name, String value) {
+			DummyModel model = createModel(name, value);
+			Memcache.put(createMemcacheKey(name), model);
+		}
+
+		@Before
+		public void setUpDatastore() {
+			putDatastore(NAME2, "200");
+		}
+
+		private void putDatastore(String name, String value) {
+			DummyModel model = createModel(name, value);
+			Datastore.put(model);
+		}
+
+		@Test
+		public void getAll() throws Exception {
+			Key[] keys = {
+					createDatastoreKey(NAME1),
+					createDatastoreKey(NAME2),
+			};
+			List<DummyModel> actual = dao.getAll(Arrays.asList(keys));
+
+			assertThat(actual, is(notNullValue()));
+			assertThat(actual.size(), is(2));
+
+			assertThat(actual.get(0).getKey(), is(createDatastoreKey(NAME1)));
+			assertThat(actual.get(1).getKey(), is(createDatastoreKey(NAME2)));
+
+			assertThat(Memcache.contains(createMemcacheKey(NAME1)), is(true));
+			assertThat(Memcache.contains(createMemcacheKey(NAME2)), is(true));
+		}
+
+	}
+
 	private static DummyModel createDefaultModel() {
-		return createModel("name", "100");
+		return createModel(DEFAULT_KEY_NAME, "100");
 	}
 
 	private static DummyModel createModel(String name, String value) {
 		DummyModel model = new DummyModel();
-		model.setKey(Datastore.createKey(DummyModel.class, name));
+		model.setKey(createDatastoreKey(name));
 		model.setData(value);
 		return model;
 	}
 
 	private static Key createDefaultDatastoreKey() {
-		return Datastore.createKey(DummyModel.class, "name");
+		return createDatastoreKey(DEFAULT_KEY_NAME);
+	}
+
+	private static Key createDatastoreKey(String name) {
+		return Datastore.createKey(DummyModel.class, name);
 	}
 
 	private static String createDefaultMemcacheKey() {
-		return dao.createMemcacheKey(createDefaultDatastoreKey());
+		return createMemcacheKey(DEFAULT_KEY_NAME);
+	}
+
+	private static String createMemcacheKey(String name) {
+		return dao.createMemcacheKey(createDatastoreKey(name));
 	}
 }
